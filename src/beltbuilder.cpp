32c6a4b73e374e3427a7c927523e3c4b0603523a
Jonathan Vice <jajvice@gmail.com>
2020-06-29 16:00:46 +0200

    Add belt stats for all non-soul pets

--- src/beltbuilder.cpp
+++ src/beltbuilder.cpp
@@ -38,14 +38,176 @@ void BeltBuilder::setBuild(QList<QPair<QString, int>> build) {
 
 QList<QPair<Stats::Stat, float>> BeltBuilder::getPetBenefits(QString pet, int stage) {
     QList<QPair<Stats::Stat, float>> benefits;
+    QList<Stats::Stat> stats;
+    float benefit;
+
+    if (pet == "Angel") {
+        benefit = 7.5 + 0.5 * stage;
+        stats.append({Stats::vit, Stats::int_, Stats::pdef, Stats::matk});
+    }
+
+    if (pet == "Baphomet") {
+        benefit = 19 + 1 * stage;
+        stats.append({Stats::int_, Stats::castSpd});
+    }
+
+    if (pet == "Blight Ogre") {
+        benefit = 9.5 + 0.5 * stage;
+        stats.append({Stats::str, Stats::int_, Stats::patk, Stats::matk});
+    }
+
+    if (pet == "Blue Pixie" || pet == "Red Pixie" || pet == "Octopus") {
+        benefit = 11 + 1 * stage;
+        stats.append({Stats::int_, Stats::matk});
+    }
+
+    if (pet == "Cerberus") {
+        benefit = 8.5 + 0.5 * stage;
+        stats.append({Stats::str, Stats::dex, Stats::patk, Stats::pacc});
+    }
+
+    if (pet == "Death Gladiator") {
+        benefit = 10.5 + 0.5 * stage;
+        stats.append({Stats::str, Stats::agi, Stats::patk, Stats::eva});
+    }
+
+    if (pet == "Death Tyrant") {
+        benefit = 9.5 + 0.5 * stage;
+        stats.append({Stats::patk, Stats::atkSpd});
+
+        // Lydians/Death Tyrants are the only pets that don't add the same benefit to all stats
+        benefits.append(QPair<Stats::Stat, float>(Stats::str, benefit * 2));
+    }
+
+    if (pet == "Drillbot") {
+        benefit = 9.5 + 0.5 * stage;
+        stats.append({Stats::vit, Stats::wis, Stats::pdef, Stats::mdef});
+    }
+
+    if (pet == "Genie") {
+        benefit = 17 + 1 * stage;
+        stats.append({Stats::vit, Stats::hpReg});
+    }
+
+    if (pet == "Gnoll") {
+        benefit = 15 + 1 * stage;
+        stats.append({Stats::vit, Stats::block});
+    }
+
+    if (pet == "Harpy") {
+        benefit = 13 + 1 * stage;
+        stats.append({Stats::dex, Stats::atkSpd});
+    }
+
+    if (pet == "Hawkman") {
+        benefit = 6.5 + 0.5 * stage;
+        stats.append({Stats::vit, Stats::str, Stats::patk, Stats::pdef});
+    }
+
+    if (pet == "Ifrit") {
+        benefit = 17 + 1 * stage;
+        stats.append({Stats::int_, Stats::mpReg});
+    }
+
+    if (pet == "Kentauros") {
+        benefit = 15 + 1 * stage;
+        stats.append({Stats::str, Stats::patk});
+    }
+
+    if (pet == "Lydian") {
+        benefit = 5.5 + 0.5 * stage;
+        stats.append({Stats::vit, Stats::str});
+
+        // Lydians/Death Tyrants are the only pets that don't add the same benefit to all stats
+        benefits.append(QPair<Stats::Stat, float>(Stats::pdef, benefit * 2));
+    }
 
     if (pet == "Minotaurus") {
-        float benefit = 9.5 + 0.5 * stage;
-        benefits.append(QPair<Stats::Stat, float>(Stats::vit, benefit));
-        benefits.append(QPair<Stats::Stat, float>(Stats::str, benefit));
-        benefits.append(QPair<Stats::Stat, float>(Stats::patk, benefit));
-        benefits.append(QPair<Stats::Stat, float>(Stats::pdef, benefit));
+        benefit = 9.5 + 0.5 * stage;
+        stats.append({Stats::vit, Stats::str, Stats::patk, Stats::pdef});
     }
+
+    if (pet == "Naga" || pet == "Ice Maiden") {
+        benefit = 15 + 1 * stage;
+        stats.append({Stats::int_, Stats::matk});
+    }
+
+    if (pet == "Nightmare") {
+        benefit = 13 + 1 * stage;
+        stats.append({Stats::dex, Stats::pacc});
+    }
+
+    if (pet == "Orc") {
+        benefit = 5.5 + 0.5 * stage;
+        stats.append({Stats::vit, Stats::str, Stats::patk, Stats::pdef});
+    }
+
+    if (pet == "Pantera") {
+        benefit = 6 + 1 * stage;
+        stats.append({Stats::str, Stats::patk});
+    }
+
+    if (pet == "Poultry") {
+        benefit = 6 + 1 * stage;
+        stats.append({Stats::str, Stats::patk});
+    }
+
+    if (pet == "Salamander") {
+        benefit = 6.5 + 0.5 * stage;
+        stats.append({Stats::str, Stats::int_, Stats::patk, Stats::matk});
+    }
+
+    if (pet == "Siren") {
+        benefit = 5.5 + 0.5 * stage;
+        stats.append({Stats::dex, Stats::wis, Stats::pacc, Stats::macc});
+    }
+
+    if (pet == "Skeleton") {
+        benefit = 11 + 1 * stage;
+        stats.append({Stats::agi, Stats::atkSpd});
+    }
+
+    if (pet == "Snowman" || pet == "Mystic Koala") {
+        benefit = 15 + 1 * stage;
+        stats.append({Stats::luck, Stats::crit});
+    }
+
+    if (pet == "Stone Golem") {
+        benefit = 17 + 1 * stage;
+        stats.append({Stats::vit, Stats::pdef});
+    }
+
+    if (pet == "Tortus") {
+        benefit = 6 + 1 * stage;
+        stats.append({Stats::vit, Stats::pdef});
+    }
+
+    if (pet == "Unicorn") {
+        benefit = 13 + 1 * stage;
+        stats.append({Stats::agi, Stats::eva});
+    }
+
+    if (pet == "Undine") {
+        benefit = 19 + 1 * stage;
+        stats.append({Stats::vit, Stats::pdef});
+    }
+
+    if (pet == "White Dragon") {
+        benefit = 9.5 + 0.5 * stage;
+        stats.append({Stats::int_, Stats::vit, Stats::matk, Stats::pdef});
+    }
+
+    if (pet == "Wolf") {
+        benefit = 11 + 1 * stage;
+        stats.append({Stats::agi, Stats::eva});
+    }
+
+    if (pet == "Yeti") {
+        benefit = 11 + 1 * stage;
+        stats.append({Stats::vit, Stats::pdef});
+    }
+
+    for (Stats::Stat stat : stats) benefits.append(QPair<Stats::Stat, float>(stat, benefit));
 
     return benefits;
 }
